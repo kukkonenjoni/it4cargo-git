4b5da3ff3ad3e1783218a689474e99525770459f
kukkonenjoni <joni.kukkonen1@gmail.com>
2024-06-26 11:53:51 +0300

    Fetch commit messages

--- main.go
+++ main.go
@@ -1,27 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	changelog "github.com/kukkonenjoni/it4cargo-git/commands"
-	"github.com/kukkonenjoni/it4cargo-git/util"
-)
+import it4cargoGit "github.com/kukkonenjoni/it4cargo-git/commands"
 
 func main() {
-	// Example: git log
-	output, err := util.RunGitCommand("log", "--pretty=format:'%s'")
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	fmt.Println("Output:\n", output)
-
-	output, err = util.RunGitCommand("pull")
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	fmt.Println("Output: ", output)
-
-	changelog.CreateChangeLog()
+	it4cargoGit.CreateChangeLog()
 }
